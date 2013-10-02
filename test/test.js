8254c2c401025193695361eff9bc38e7ca482f33
jjt <jason@jasontrill.com>
2013-10-02 15:01:34 -0700

    Updated README and test

--- test/test.js
+++ test/test.js
@@ -2,14 +2,12 @@ var input = 'test@animalonymous.com',
     anml = require('../'),
     salt = "saltysalt111",
     results = [
-      "Input: " + input,
+      "input: " + input,
       "hash(input): [" + anml.hash(input) + "]",
       "hash(input, \"saltysalt111\"): [" + anml.hash(input, salt) + "]",
       "hashStr(input): " + anml.hashStr(input),
       "random(): [" + anml.random() + "]",
       "randomStr(): " + anml.randomStr()
     ];
-
-
 
 console.log(results.join("\n"));
